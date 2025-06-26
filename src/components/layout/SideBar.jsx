7514679e1a28eefcb66df534490a1dbcab31259f
Francesco Giatti <giatti.francesco@gmail.com>
2025-06-26 11:17:13 +0200

    aggiunto collegamento git

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -8,7 +8,7 @@ const SideBar = () => {
         <div className='w-[8vh] h-[100%] flex flex-col gap-[1vh] items-center'>
             <button  className={`bg-[#484B59] cursor-pointer hover:bg-[#484B5970] h-[5vh] w-[5vh] rounded-[0.7vh] flex justify-center items-center`} ><FiHome className='text-white text-[2.5vh]'></FiHome></button>
             <button onClick={() =>  window.open("https://github.com/36fg", '_blank')} className={` cursor-pointer hover:bg-[#484B5970] h-[5vh] w-[5vh] rounded-[0.7vh] flex justify-center items-center`} ><FiGithub className='text-white text-[2.5vh]'></FiGithub></button>
-            <button  className={`cursor-pointer hover:bg-[#484B5970] h-[5vh] w-[5vh] rounded-[0.7vh] flex justify-center items-center`} ><IoStarOutline className='text-white text-[2.5vh]'></IoStarOutline></button>
+            <button onClick={() => window.open("https://github.com/36fg/vscode-portfolio-clone", '_blank')} className={`cursor-pointer hover:bg-[#484B5970] h-[5vh] w-[5vh] rounded-[0.7vh] flex justify-center items-center`} ><IoStarOutline className='text-white text-[2.5vh]'></IoStarOutline></button>
         </div>
     )
 }
